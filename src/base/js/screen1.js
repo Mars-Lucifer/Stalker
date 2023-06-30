7027f72461638a3aee3e5942cc8ea15f08e5da3d
Mars-Lucifer <89739852+Mars-Lucifer@users.noreply.github.com>
2023-06-30 14:04:24 +0300

    screen 1+

--- src/base/js/screen1.js
+++ src/base/js/screen1.js
@@ -6,8 +6,8 @@ const ScreenOne = () => {
         document.addEventListener('mousemove', e => {
             Object.assign(document.documentElement, {
                 style: `
-                --moveX: ${e.clientX}deg;
-                --moveY: ${e.clientY}deg;
+                --moveX: ${(e.clientX - window.innerWidth / 2) * -0.003}deg;
+                --moveY: ${(e.clientY - window.innerHeight / 2) * -0.006}deg;
                 `
             })
         })
@@ -15,14 +15,16 @@ const ScreenOne = () => {
 
     return (
         <div className='Screen'>
-            <div className='container'>
-                <h3>Hello! It's the testing</h3>
-            </div>
-
             <div className='Screen__bac'>
                 <div className='bac__layers'>
                     <div className='layers__item item_1'></div>
-                    <div className='layers__item item_2'></div>
+                    <div className='layers__item item_2'>
+                        <div>
+                            <p className='text__main_big'>Welcome to the zone, stalker</p>
+                            <p className='text__main_small'>    -Listen, stalker, here we have the site of our Main server. Come visit us and you will find many interesting things. We've already done some stuff here, trust me. You can check us out if you're not afraid of the Zone.</p>
+                        </div>
+                        <a className='link__main' target="blank" href='https://www.youtube.com/watch?v=2tAPxjcGLms&t=2319s'>server</a>
+                    </div>
                 </div>
             </div>
         </div>
